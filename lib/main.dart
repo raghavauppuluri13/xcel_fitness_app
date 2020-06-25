7ed08123580cf58038ce9fea41a3808257368460
adi-kulkarni1 <61598391+adi-kulkarni1@users.noreply.github.com>
2020-06-25 00:32:16 -0500

    Completed Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -41,80 +41,146 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
-      TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0, color: Colors.white);
 
-      @override
-      Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final passwordField = TextField(
+      
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
 
-        final emailField = TextField(
-          obscureText: false,
-          style: style,
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-              hintText: "Email",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-        );
-        final passwordField = TextField(
-          obscureText: true,
-          style: style,
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-              hintText: "Password",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-        );
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xffc70101),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+        onPressed: () {},
+        child: Text("Sign In",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
 
-        final loginButon = Material(
-          elevation: 5.0,
-          borderRadius: BorderRadius.circular(30.0),
-          color: Color(0xffc70101),
-          child: MaterialButton(
-            minWidth: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-            onPressed: () {},
-            child: Text("Sign In",
-                textAlign: TextAlign.center,
-                style: style.copyWith(
-                    color: Colors.white, fontWeight: FontWeight.bold)),
+    final signupButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xffc70101),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+        onPressed: () {},
+        child: Text("Sign Up",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/FoodImageApp.jpg'),
+            fit: BoxFit.cover,
           ),
-        );
-        
-        return Scaffold(
-          body: Center(
-            child: Container(
-              color: Colors.white,
+        ),
+        child: Center(
+          child: Container(
               child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 155.0,
-                      child: Image.asset(
-                      'assets/FoodImageApp.jpg',
-                        fit: BoxFit.contain,
-                      ),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 0.0),
+                    child: SizedBox(
+                      width: 350.0,
+                      child: emailField,
                     ),
-                    SizedBox(height: 25.0),
-                    emailField,
-                    SizedBox(height: 15.0),
-                    passwordField,
-                    SizedBox(
-                      height: 15.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 25.0),
+                    child: SizedBox(
+                      width: 350.0,
+                      child: passwordField,
                     ),
-                    loginButon,
-                    SizedBox(
-                      height: 15.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15.0),
+                    child: SizedBox(
+                      width: 200.0,
+                      child: loginButton,
                     ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      }
-    }
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Forgot login details? ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            height: 2.0,
+                            fontSize: 15.0,
+                        ),
+                        ),
+                        TextSpan(
+                          text: 'Reset Password',
+                          style: TextStyle(
+                            color: Colors.white,
+                            height: 2.0,
+                            fontSize: 15.0,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '\n\n\n', 
+                        ),
+                        
+                        TextSpan(
+                          text: '-------------------------------or-------------------------------',
+                          style: TextStyle(
+                            color: Colors.white,
+                            height: 2.0,
+                            fontSize: 20.0,
+                        ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15.0),
+                    child: SizedBox(
+                      width: 400.0,
+                      child: signupButton,
+                    ),
+                  ),
+                ]),
+          )),
+        ),
+      ),
+    );
+  }
+}
