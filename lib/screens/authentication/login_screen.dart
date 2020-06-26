9182be8a37fd46c41abafcb8dcc5dab5f13847e4
raghavauppuluri13 <sairagupp13@gmail.com>
2020-06-26 16:13:40 -0500

    Made some file layout changes and made some edits on the login screen

---
+++ lib/screens/authentication/login_screen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextStyle style =
+      TextStyle(fontFamily: 'Montserrat', fontSize: 20.0, color: Colors.white);
+
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextField(
+        obscureText: false,
+        style: style,
+        decoration: new InputDecoration(
+          fillColor: Colors.white,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0),
+              borderSide: BorderSide(color: Colors.blue)),
+          filled: true,
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+        ));
+    final passwordField = TextField(
+        obscureText: true,
+        style: style,
+        decoration: new InputDecoration(
+          fillColor: Colors.white,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0),
+              borderSide: BorderSide(color: Colors.blue)),
+          filled: true,
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+        ));
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xffc70101),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+        onPressed: () {},
+        child: Text("Sign In",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    final signupButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xffc70101),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+        onPressed: () {},
+        child: Text("Sign Up",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+        ),
+        child: Center(
+          child: Container(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: SizedBox(
+                      width: 300.0,
+                      child: Image(
+                        image: AssetImage('assets/images/XF.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 0.0),
+                    child: SizedBox(
+                      width: 350.0,
+                      child: emailField,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 25.0),
+                    child: SizedBox(
+                      width: 350.0,
+                      child: passwordField,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15.0),
+                    child: SizedBox(
+                      width: 200.0,
+                      child: loginButton,
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Forgot login details? ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            height: 2.0,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Reset Password',
+                          style: TextStyle(
+                            color: Colors.white,
+                            height: 2.0,
+                            fontSize: 15.0,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15.0),
+                    child: SizedBox(
+                      width: 400.0,
+                      child: signupButton,
+                    ),
+                  ),
+                ]),
+          )),
+        ),
+      ),
+    );
+  }
+}
